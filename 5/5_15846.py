f44a2a3983eb390102f25c4e70fd462d3d487f4a
i1zh1 <marmok21101979@gmail.com>
2025-10-21 18:54:57 +0400

    -

--- 5/5_15846.py
+++ 5/5_15846.py
@@ -1,9 +1,10 @@
 inp = int(input())
 a = list(map(int, bin(inp)[2:]))
-b=0
 if inp % 2 == 0:
     a += [0, 0]
 elif inp % 2 != 0:
     a += [1, 1]
 print(int(''.join(map(str, a)), 2))
+
+
 #119 подбором, за 5 минут. ответ 29 в этом случае
