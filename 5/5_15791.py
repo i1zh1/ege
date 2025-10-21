9893b70852b1acd3d88c1afd5c438591cf7f06a9
i1zh1 <marmok21101979@gmail.com>
2025-10-21 18:32:47 +0400

    adding task 5

---
+++ 5/5_15791.py
@@ -0,0 +1,13 @@
+a = list(map(int, bin(int(input()))[2:]))
+b=0
+for i in a: 
+    b+=i
+a.append(b%2)
+    
+b=0
+for i in a:
+    b+=i
+a.append(b%2)
+
+print(int(''.join(map(str, a)), 2))
+# ответ 102 подбором, решение заняло 9 минут(плохо, надо решать еще)
