d11aedf66d4053b5492bbd22897f1c245176c8f3
i1zh1 <marmok21101979@gmail.com>
2025-11-10 21:01:31 +0400

    fixing

--- 17/17_37337.py
+++ 17/17_37337.py
@@ -7,11 +7,11 @@ max_sum = 0
 
 for i in range(len(numb)-1):
     a = numb[i]
-    b = numb[i-1]
+    b = numb[i+1]
     if a%160 != b%160 and a%7==0 or b%7==0:
         count += 1
         if a + b > max_sum:
-            max_sum += a+b
+            max_sum = a+b
 
 print(count)
 print(max_sum)
