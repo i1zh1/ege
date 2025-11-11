f368c0e0a8fcaa22936dfb42944a66667c89dbc3
i1zh1 <marmok21101979@gmail.com>
2025-11-11 22:55:24 +0400

    adding task 27

---
+++ 27/examples/27424/main.py
@@ -0,0 +1,22 @@
+#РЕШЕНИЕ С "РЕШУ ЕГЭ" для собственного разбора
+
+f = open("27/examples/27424/27-B_demo.txt")  # для файла A замените название
+s = f.readlines()
+'''
+как бы я сделал:
+with open("27-B_demo.txt") as f:
+    s = f.readlines()
+    [  код  ]
+'''
+n = int(s[0])  # количество пар
+summi = 0
+d = 10**6
+for i in range(1, n + 1):
+    x, y = map(int, s[i].split())
+    summi += max(x, y)
+    if abs(x - y) % 3 != 0:
+        d = min(d, abs(x - y))
+if summi % 3 != 0:
+    print(summi)
+else:
+    print(summi - d)
