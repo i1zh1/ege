2fc41e9e2b2b986683cd077010f617e139fd6653
i1zh1 <marmok21101979@gmail.com>
2025-11-11 20:56:22 +0400

    adding task 2

---
+++ 2/2_example.py
@@ -0,0 +1,9 @@
+print('x y z w')
+for x in range(0, 2):
+    for y in range(0, 2):
+        for z in range(0, 2):
+            for w in range(0, 2):
+                if not(((x <= y) and (y <= w)) or (z == (x or y))):
+                    print(x, y, z, w)
+
+# для всех заданий одно и тоже
